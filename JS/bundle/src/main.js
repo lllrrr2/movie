7ceb16830da06cef67c1b08150011fbab33fcc74
d1y <chenhonzhou@gmail.com>
2025-09-25 17:25:00 +0800

    fix: JS/bundle async getM3u8WithStr
    不应该将 getM3u8WithStr 标记为async函数

--- JS/bundle/src/main.js
+++ JS/bundle/src/main.js
@@ -11,7 +11,7 @@ const utils = {
     const html = await req(`${env.baseUrl}${iframe}`)
     return this.getM3u8WithStr(html)
   },
-  async getM3u8WithStr(str) {
+  getM3u8WithStr(str) {
     const m3u8 = str.match(/"url"\s*:\s*"([^"]+\.m3u8)"/)[1]
     const realM3u8 = m3u8.replaceAll("\\/", "/")
     return realM3u8
